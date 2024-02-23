ad89994f68f10e8c4bbaf4d5eddb2050a48bf5e9
Biden2020prez <106286969+biden2020prez@users.noreply.github.com>
2024-02-23 15:54:12 -0500

    Added /python botmodule

---
+++ gmbotmodules/python.py
@@ -0,0 +1,23 @@
+from gmbotmodules import logusage, sendMessage
+import asyncio
+import pyston
+
+
+
+only_messages_with_text = True
+moduleName = 'Python'
+helpString = '/python <script> will return the output'
+
+async def send_output(script, message):
+    client = pyston.PystonClient()
+    output = await client.execute("python", [pyston.File(script)])
+    sendMessage(message=message, text=str(output))
+    return output
+
+def checkMessage(message):
+    if message['text'].strip().casefold().startswith('/python'.strip().casefold()):
+        logusage()
+        output = asyncio.run(send_output(message['text'].strip()[7:], message))
+        return True
+    else: 
+        return False
